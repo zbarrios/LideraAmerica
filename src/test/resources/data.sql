04daa55d39cbdf5c0d84dd2616e609b09595cf57
Carlos Barrios <cabarriosb@gmail.com>
2023-07-26 08:52:54 -0500

    Fix Tests

--- src/test/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO AUTHOR (author_name) VALUES ('Author One'), ('Author Two');
-INSERT INTO BOOK (title,author_id) VALUES ('Book One',1), ('Book Two',1), ('Book Three',1), ('Book Four',2), ('Book Five',2);
-INSERT INTO ROLE (role_name) VALUES('ROLE_ADMIN'),('ROLE_USER');
